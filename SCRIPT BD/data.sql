d68a8c04d1256c7fcf09999732a83a79819e9452
Eduardo Castillo <ecm1512@hotmail.com>
2020-07-15 00:51:39 -0500

    Agrege data sql

---
+++ SCRIPT BD/data.sql
@@ -0,0 +1,61 @@
+INSERT INTO `peliculas` VALUES (3,'¡Scooby!',1,'SCOOBY nos cuenta como se conocieron los amigos Scooby y Shaggy y cómo se unieron a los jóvenes detectives Fred, Velma y Daphne para formar la famosa agencia de detectives Mystery Inc. Ahora, con cientos de casos resueltos y aventuras compartidas, Scooby y la pandilla se enfrentan al misterio más gigantesco y complicado de sus carreras: un complot para que el perro fantasma Cerbero campe a sus anchas por el mundo. Mientras corren a toda prisa para detener esta \"perroapocalipsis\" mundial, la pandilla descubre que Scooby tiene un legado secreto y un destino épico mayor de lo que nadie imaginaba.','/vista/usuario/CARTELERA/imagenes/scooby.jpg',1,1),(4,'Mulan',1,'China, en la época de la dinastía Han, Mulan es una joven inquieta y rebelde, hija de un respetable veterano de guerra. La joven siempre se mete en líos por no ser exactamente una dama sofisticada y, de hecho, no le ha dado muy buena impresión a la casamentera, que evalúa a las jóvenes como potenciales novias. Cuando estalla la guerra contra los hunos, los ciudadanos empiezan a ser reclutados, ya que el Emperador decide formar un ejército compuesto por un varón de cada familia. Es entonces cuando Mulan se hace pasar por un guerrero, para evitar que su anciano padre tenga que ir al frente. Así, disfrazada de hombre, logra ocupar su puesto y marchar a la guerra, donde demostrará su talento y luchará valientemente junto al ejército imperial.','/vista/usuario/CARTELERA/imagenes/mulan.jpeg',1,2),(5,'365 days',2,'En 365 DNI Massimo Torricelli, el jefe joven y poderoso de una familia mafiosa en Sicilia, no tiene otra alternativa que hacerse cargo de la compañía familiar cuando se asesina a su padre. Mientras, una directiva de ventas, Laura Biel, tiene mucho éxito en su vida profesional mas su vida personal es otra historia. Su relación es tensa y Laura quiere que la pasión altere la monotonía de sus días. En un intento de salvar su relación, Laura, su novio y sus amigos vuelan a Sicilia. No obstante, se alteran de forma trágica sus vacaciones cuando a Laura la recoge Massimo, quien a ella le da trescientos sesenta y cinco días para enamorarse de él. ‘365 dni’ es un drama erótica polaco que está basado en la novela de Blanka Lipinska.','/vista/usuario/CARTELERA/imagenes/365days.jpg',1,3),(6,'Bad Boys Para Siempre',3,'Miami vuelve a ser el escenario de esta nueva entrega de Dos policías rebeldes. Allí, los detectives Mike Lowrey (Will Smith) y Marcus Burnett (Martin Lawrence) vuelven a hacer de las suyas, y su nueva aventura volverá a estar plagada de acción, bandas, persecuciones de coches y explosiones. Con unos métodos únicos y un peculiar sentido de afrontar los problemas y el peligro que abunda en las calles, incluidas las constantes discusiones entre ambos, este par de policías afrontará nuevos retos y siempre estarán preparados para saltarse las normas cuando la justicia lo requiera.','/vista/usuario/CARTELERA/imagenes/badboys.jpg',1,4);
+
+INSERT INTO `butacas` VALUES (1,1,'A',1,1),(2,1,'A',2,1),(3,1,'A',12,1),(4,1,'A',13,1),(5,1,'B',1,1),(6,1,'B',2,1),(7,1,'B',3,1),(8,1,'B',4,1),(9,1,'B',5,1),(10,1,'B',6,1),(11,1,'B',7,1),(12,1,'B',8,1),(13,1,'B',9,1),(14,1,'B',10,1),(15,1,'B',11,1),(16,1,'B',12,1),(17,1,'B',13,1),(18,1,'C',1,1),(19,1,'C',2,1),(20,1,'C',3,1),(21,1,'C',4,1),(22,1,'C',5,1),(23,1,'C',6,1),(24,1,'C',7,1),(25,1,'C',8,1),(26,1,'C',9,1),(27,1,'C',10,1),(28,1,'C',11,1),(29,1,'C',12,1),(30,1,'C',13,1),(31,1,'D',1,1),(32,1,'D',2,1),(33,1,'D',3,1),(34,1,'D',4,1),(35,1,'D',5,1),(36,1,'D',6,1),(37,1,'D',7,1),(38,1,'D',8,1),(39,1,'D',9,1),(40,1,'D',10,1),(41,1,'D',11,1),(42,1,'D',12,1),(43,1,'D',13,1),(44,1,'E',1,1),(45,1,'E',2,1),(46,1,'E',3,1),(47,1,'E',4,1),(48,1,'E',5,1),(49,1,'E',6,1),(50,1,'E',7,1),(51,1,'E',8,1),(52,1,'E',9,1),(53,1,'E',10,1),(54,1,'E',11,1),(55,1,'E',12,1),(56,1,'E',13,1),(57,1,'F',1,1),(58,1,'F',2,1),(59,1,'F',3,1),(60,1,'F',4,1),(61,1,'F',5,1),(62,1,'F',6,1),(63,1,'F',7,1),(64,1,'F',8,1),(65,1,'F',9,1),(66,1,'F',10,1),(67,1,'F',11,1),(68,1,'F',12,1),(69,1,'F',13,1),(70,1,'G',1,1),(71,1,'G',2,1),(72,1,'G',3,1),(73,1,'G',4,1),(74,1,'G',5,1),(75,1,'G',6,1),(76,1,'G',7,1),(77,1,'G',8,1),(78,1,'G',9,1),(79,1,'G',10,1),(80,1,'G',11,1),(81,1,'G',12,1),(82,1,'G',13,1),(83,1,'H',1,1),(84,1,'H',2,1),(85,1,'H',3,1),(86,1,'H',4,1),(87,1,'H',5,1),(88,1,'H',6,1),(89,1,'H',7,1),(90,1,'H',8,1),(91,1,'H',9,1),(92,1,'H',10,1),(93,1,'H',11,1),(94,1,'H',12,1),(95,1,'H',13,1),(96,1,'I',1,1),(97,1,'I',2,1),(98,1,'I',3,1),(99,1,'I',4,1),(100,1,'I',5,1),(101,1,'I',6,1),(102,1,'I',7,1),(103,1,'I',8,1),(104,1,'I',9,1),(105,1,'I',10,1),(106,1,'I',11,1),(107,1,'I',12,1),(108,1,'I',13,1),(109,1,'J',1,1),(110,1,'J',2,1),(111,1,'J',3,1),(112,1,'J',4,1),(113,1,'J',5,1),(114,1,'J',6,1),(115,1,'J',7,1),(116,1,'J',8,1),(117,1,'J',9,1),(118,1,'J',10,1),(119,1,'J',11,1),(120,1,'J',12,1),(121,1,'J',13,1),(122,1,'K',1,1),(123,1,'K',2,1),(124,1,'K',3,1),(125,1,'K',4,1),(126,1,'K',5,1),(127,1,'K',6,1),(128,1,'K',7,1),(129,1,'K',8,1),(130,1,'K',9,1),(131,1,'K',10,1),(132,1,'K',11,1),(133,1,'K',12,1),(134,1,'K',13,1),(135,1,'L',1,1),(136,1,'L',2,1),(137,1,'L',3,1),(138,1,'L',4,1),(139,1,'L',5,1),(140,1,'L',6,1),(141,1,'L',7,1),(142,1,'L',8,1),(143,1,'L',9,1),(144,1,'L',10,1),(145,1,'L',11,1),(146,1,'L',12,1),(147,1,'L',13,1),(148,1,'M',1,1),(149,1,'M',2,1),(150,1,'M',3,1),(151,1,'M',4,1),(152,1,'M',5,1),(153,1,'M',6,1),(154,1,'M',7,1),(155,1,'M',8,1),(156,1,'M',9,1),(157,1,'M',10,1),(158,1,'M',11,1),(159,1,'M',12,1),(160,1,'M',13,1),(161,1,'M',14,1),(162,1,'M',15,1);
+
+INSERT INTO `cartelera` VALUES (1,3,'2020-07-01','2020-10-01'),(2,4,'2020-07-01','2020-10-01'),(3,5,'2020-07-01','2020-10-01'),(4,6,'2020-07-01','2020-10-01');
+
+
+INSERT INTO `categoriapeliculas` VALUES (1,'Infantiles','Pelicula apta para niños'),(2,'Drama','Pelicula donde se abordan temas graves y trascendentes con el menor humor posible'),(3,'Accion','Género cinematográfico donde prima la espectacularidad de las imágenes.');
+
+
+insert into salas values (1,162,1);
+insert into salas values (2,162,1);
+insert into salas values (3,162,2);
+insert into salas values (4,162,2);
+
+
+
+insert into detallecartelera (idCartelera,idHorario,idSala) values (1,1,1);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (1,2,1);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (1,3,1);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (1,4,1);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (1,5,1);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (1,6,1);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (1,7,1);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (1,8,1);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (1,9,1);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (1,10,1);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (1,11,1);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (2,1,2);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (2,2,2);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (2,3,2);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (2,4,2);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (2,5,2);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (2,6,2);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (2,7,2);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (2,8,2);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (2,9,2);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (2,10,2);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (2,11,2);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (3,1,3);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (3,2,3);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (3,3,3);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (3,4,3);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (3,5,3);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (3,6,3);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (3,7,3);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (3,8,3);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (3,9,3);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (3,10,3);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (3,11,3);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (4,1,4);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (4,2,4);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (4,3,4);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (4,4,4);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (4,5,4);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (4,6,4);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (4,7,4);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (4,8,4);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (4,9,4);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (4,10,4);
+insert into detallecartelera (idCartelera,idHorario,idSala) values (4,11,4);
